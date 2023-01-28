df7b50f514d62f532f7ef2e70d2ffaf2708a051a
Grey03 <greyeag3@gmail.com>
2023-01-28 18:47:26 -0500

    Moved files again, made folders more professional

--- Pyrchive.py
+++ Pyrchive.py
@@ -1,58 +1,138 @@
-import datetime, json, os
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-jsonsLocation = __location__ + "/files/data"
+import datetime, json, os, tkinter.messagebox, time
 
-class ArchiveManager:
+class archivemanager:
     def __init__(self):
+        self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self.jsonsLocation = self.__location__ + "\\pyrchiveFolders\\pyrchiveJsonData\\"
         self.openFilesImmediately = True
         self.openMenuImmediately = True
         self.localFiles = True
-    class TagGroup:
+    class taggroup:
         def __init__(self):
+            #tag groups should be a grouping of tags that are similar to each other
             self.name = ""
             self.tags = []
             self.description = ""
             self.color = "cyan"
-    class ArchiveEntry:
+    class archiveentry:
         def __init__(self, **kwargs):
+            #This is the basic entry, with all the attributes
             self.ID = -1
             self.title = ""
             self.creator = "" 
             self.tags = []
-            self.data = ""
-            self.misc_notes = ""
-            self.upload_date = (datetime.datetime.now().__str__())
+            self.fileLocation = ""
+            self.notes = ""
+            self.uploadDate = (datetime.datetime.now().__str__())
             for key, value in kwargs.items():
                 if hasattr(self, key):
-                    setattr(self, key, value) 
-        def filter(self, filterList):
-            positiveTerms = []
-            negativeTerms = []
-            for term in filterList:
-                if str(term).lower().find("-", 0, 1) != -1:
-                    negativeTerms.append(str(term.replace("-","")).lower())
+                    setattr(self, key, value)
+        def __str__(self):
+            #Returns a string representation of the dictionary of the object
+            return json.dumps(self.__dict__(), indent=4, sort_keys=True)
+        def __dict__(self):
+            #Should return a dictionary that represent the entry
+            return  {
+                    "ID": self.ID,
+                    "title": self.title,
+                    "creator": self.creator,
+                    "tags": self.tags,
+                    "fileLocation" : self.fileLocation,
+                    "notes": self.notes,
+                    "uploadDate": self.uploadDate
+                    }
+        def hasTags(self, filterList):
+            positiveList = []
+            negativeList = []
+            for filterWord in filterList:
+                if filterWord.startswith("-"):
+                    negativeList.append(filterWord.lower().replace("-", ""))
                 else:
-                    positiveTerms.append(str(term).lower())
-
-            if(len(negativeTerms) > 0):
-                for negTerm in negativeTerms:
-                    if str(negTerm) in self.tags:
-                        return False
-            if(len(positiveTerms) > 0):
-                for posTerm in positiveTerms:
-                    if str(posTerm) not in self.tags:
-                        return False
+                    positiveList.append(filterWord.lower())
+            for negativeTerm in negativeList:
+                if negativeTerm in self.tags:
+                    return False
+            for positiveTag in positiveList:
+                if positiveTag not in self.tags:
+                    return False
             return True
-    def getEntryFromJson(fileID):
-        print (jsonsLocation + "/ArchiveEntries.json")
+    def getNewID(self):
+        #Looks for any unused ID in the database to reuse and returns a new one if one is not found.
         try:
-            with open(jsonsLocation + "/ArchiveEntries.json", "r") as jsonFile:
+            for idx, file in enumerate(self.getEntries()):
+                if idx != file["ID"]: return idx
+            return len(self.getEntries())
+        except:
+            return 0
+    def getEntryByID(self, ID):
+        #Gets the entries from the json, then tries to return the specific entry asked for
+        try:
+            for file in self.getEntries():
+                if file["ID"] == ID: return file
+            return None
+        except: tkinter.messagebox.showerror("Error", "Could not find entry with ID " + str(ID))
+    def getEntries(self):
+        #Returns a list of dictionaries that represent all the entries in the archive
+        try:
+            with open(self.jsonsLocation + "/ArchiveEntries.json", "r") as jsonFile:
                 return json.load(jsonFile)
         except:
-            return None
-    def saveEntryToJson(fildInfo):
-        print("Saving entry")
-        
-temp = ArchiveManager()
+            return []
+    def filterEntries(self, filterList, start, filterCount):
+        entries = self.getEntries()
+        if filterCount == -1: filterCount = len(entries)
+        foundFiles = []
+        timeToSort = time.time()
+        for entryLocation in range(start, len(entries)):
+            entry = self.archiveentry(**entries[entryLocation])
+            if entry.hasTags(filterList):
+                foundFiles.append(entry)
+            if len(foundFiles) >= filterCount:
+                break
+        return {"files": foundFiles,
+            "sortTime": time.time()-timeToSort}
+    def saveEntry(self, file):
+        #First checks to see if the file is a dictionary because this should only function with the dictionary format of an entry.
+        saveTime = time.time()
+        if type(file) != dict:
+            raise ValueError("File must be a dictionary")
+        #Loads entire list of entries into a list
+        if len(self.getEntries()) > 0:
+            entriesList = self.getEntries()
+        else:
+            entriesList = []
+        #This controls how its modified, aka where a new file should be placed or an edit that should be made
+        if len(entriesList)> file["ID"] and entriesList[file["ID"]]["ID"] == file["ID"]:
+            entriesList[file["ID"]] = file
+        elif len(entriesList)> file["ID"]:
+            entriesList.insert(file["ID"],file)
+        else:
+            entriesList.append(file)
+        try:
+            with open(self.jsonsLocation + "/ArchiveEntries.json", "w") as jsonFile:
+                json.dump(entriesList, jsonFile,indent=4)
+        except:
+            raise tkinter.messagebox.showerror(title="Error", message=("Could not save"))
+        return (time.time()-saveTime)
+    def createTestEntry(self, **kwargs):
+        #Allows me to quickly create a new entry for testing purposes    
+        ID=self.getNewID()
+        for key, value in kwargs.items():
+            if key == "ID" and value <= self.getNewID(): ID = value
+        newEntry = self.archiveentry(ID=ID, title="title", creator="creator", tags=["tag1", "tag2", "tag3"], fileLocation = "fileLocation", notes = "notes")
+        self.saveEntry(newEntry.__dict__())
 
-print (ArchiveManager.getFileFromJson(9))
+
+        
+temp = archivemanager()
+
+creatingFilesTime = time.time()
+
+for i in range(1):
+    temp.createTestEntry()
+print (time.time()-creatingFilesTime)
+
+
+x = temp.filterEntries(["tag1", "cat"], 0, -1)
+for entry in x["files"]:
+    print (entry, x["sortTime"])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter, os, math, random, shutil, json
-from Pyrchive import ArchiveManager
+from pyrchive import archivemanager
 from tkinter import filedialog, messagebox
 from tktooltip import ToolTip
 #from PIL import Image
@@ -16,68 +16,59 @@ class App(customtkinter.CTk):
         super().__init__()
         self.title("Tarchive")
         self.geometry(f"{1135}x{925}")
-        
-        self.Archive = ArchiveManager()
+        self.Archive = archivemanager()
         self.pageIndex = 0
 
-        #Top Buttons Frame
-        self.optionButtonsFrame=customtkinter.CTkFrame(self)
-        #-+-+-+-+-+-
-        self.settingsButton=customtkinter.CTkButton(self.optionButtonsFrame, text="Settings")
-        self.settingsButton.pack(side="left", padx=5,pady=5)
-        self.uploadButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Upload")
-        self.uploadButton.pack(side="left", padx=5,pady=5)
-        self.savedSearchesDropdown=customtkinter.CTkOptionMenu(self.optionButtonsFrame, values=["Saved Searches: "])
-        self.savedSearchesDropdown.pack(side="left", padx=5,pady=5)
-        self.showAllTagsButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Show All Tags")
-        self.showAllTagsButton.pack(side="left", padx=5,pady=5)
-        self.randomButton=customtkinter.CTkButton(self.optionButtonsFrame, text= "Random")
-        self.randomButton.pack(side="left",padx=5,pady=5)
-        self.refreshButton=customtkinter.CTkButton(self.optionButtonsFrame, text=r"↻", width=10, height=10)
-        self.refreshButton.pack(side="right",padx=5,pady=5)
-        self.fileCountLabel=customtkinter.CTkLabel(self.optionButtonsFrame,text=f"Total File Count: {len(self.Archive.archiveList)}")
-        self.fileCountLabel.pack(side="left",padx=5,pady=5)
-        self.optionButtonsFrame.pack(padx=10, pady=5, fill="x")
-
-        #Search Frame
-        self.searchFrame = customtkinter.CTkFrame(self)
-        self.saveButton = customtkinter.CTkButton(self.searchFrame, text="Save",width=50)
-        self.saveButton.pack(side="left")
-        self.searchBar= customtkinter.CTkEntry(self.searchFrame,placeholder_text="Search")
-        self.searchBar.bind("<Return>")
-        self.searchBar.pack(padx=5,side="left", fill="x",expand=True)
-        self.searchButton = customtkinter.CTkButton(self.searchFrame, text="Search", width=80)
-        self.searchButton.pack(side="left")
-        self.searchFrame.pack(padx=10, pady=5,fill="x")
-
-        #Bottom Frame
-        self.bottomFrame=customtkinter.CTkFrame(self, fg_color="transparent")
-        self.tagFrame=customtkinter.CTkFrame(self.bottomFrame, width=200)
-        self.tagLabel=customtkinter.CTkLabel(self.tagFrame, text="Tags", font=("Roboto", 13, "bold"))
-        self.tagLabel.pack(padx=5,pady=0)
-        self.tagList = customtkinter.CTkFrame(self.tagFrame)
-        self.tagList.pack(padx=5,pady=5, expand=True, fill="both")
-        self.tagFrame.pack(side="left", fill="y")
-        self.entryFrame=customtkinter.CTkFrame(self.bottomFrame, fg_color="transparent")
-        self.entryFrame.pack(side="left",fill="both", expand=True)
-        self.bottomFrame.pack(fill="both",expand=True,padx=10,pady=5)
-
-        #Put the page buttons on the browse frame
-        #self.pageButtonFrame = customtkinter.CTkFrame(self, fg_color="transparent")
-        #self.pageLeft = customtkinter.CTkButton(self.pageButtonFrame, text="Previous")
-        #self.pageLeft.grid(row=0, column=0)
-        #self.currentPageLabel = customtkinter.CTkLabel(self.pageButtonFrame, text=f"Page: {self.pageIndex+1}")
-        #self.currentPageLabel.grid(row=0, column=1)
-        #self.pageRight = customtkinter.CTkButton(self.pageButtonFrame, text="Next")
-        #self.pageRight.grid(row=0, column=2)
-        #self.pageButtonFrame.pack()
+        def TopFrame():
+            #Top Buttons Frame
+            self.optionButtonsFrame=customtkinter.CTkFrame(self)
+            #-+-+-+-+-+-
+            self.settingsButton=customtkinter.CTkButton(self.optionButtonsFrame, text="Settings")
+            self.settingsButton.pack(side="left", padx=5,pady=5)
+            self.uploadButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Upload")
+            self.uploadButton.pack(side="left", padx=5,pady=5)
+            self.savedSearchesDropdown=customtkinter.CTkOptionMenu(self.optionButtonsFrame, values=["Saved Searches: "])
+            self.savedSearchesDropdown.pack(side="left", padx=5,pady=5)
+            self.showAllTagsButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Show All Tags")
+            self.showAllTagsButton.pack(side="left", padx=5,pady=5)
+            self.randomButton=customtkinter.CTkButton(self.optionButtonsFrame, text= "Random")
+            self.randomButton.pack(side="left",padx=5,pady=5)
+            self.refreshButton=customtkinter.CTkButton(self.optionButtonsFrame, text=r"↻", width=10, height=10)
+            self.refreshButton.pack(side="right",padx=5,pady=5)
+            self.fileCountLabel=customtkinter.CTkLabel(self.optionButtonsFrame,text=f"Total File Count: {9}")
+            self.fileCountLabel.pack(side="left",padx=5,pady=5)
+            self.optionButtonsFrame.pack(padx=10, pady=5, fill="x")
+        TopFrame()
+        def SearchFrame():
+            #Search Frame
+            self.searchFrame = customtkinter.CTkFrame(self)
+            self.saveButton = customtkinter.CTkButton(self.searchFrame, text="Save",width=50)
+            self.saveButton.pack(side="left")
+            self.searchBar= customtkinter.CTkEntry(self.searchFrame,placeholder_text="Search")
+            self.searchBar.bind("<Return>")
+            self.searchBar.pack(padx=5,side="left", fill="x",expand=True)
+            self.searchButton = customtkinter.CTkButton(self.searchFrame, text="Search", width=80)
+            self.searchButton.pack(side="left")
+            self.searchFrame.pack(padx=10, pady=5,fill="x")
+        SearchFrame()
+        def BottomFrame():
+            #Bottom Frame
+            self.bottomFrame=customtkinter.CTkFrame(self, fg_color="transparent")
+            self.tagFrame=customtkinter.CTkFrame(self.bottomFrame, width=200)
+            self.tagLabel=customtkinter.CTkLabel(self.tagFrame, text="Tags", font=("Roboto", 13, "bold"))
+            self.tagLabel.pack(padx=5,pady=0)
+            self.tagList = customtkinter.CTkFrame(self.tagFrame)
+            self.tagList.pack(padx=5,pady=5, expand=True, fill="both")
+            self.tagFrame.pack(side="left", fill="y")
+            self.entryFrame=customtkinter.CTkFrame(self.bottomFrame, fg_color="transparent")
+            self.entryFrame.pack(side="left",fill="both", expand=True)
+            self.bottomFrame.pack(fill="both",expand=True,padx=10,pady=5)
+        BottomFrame()
+        
     def clearEntryFrame(self):
         print ("clearEntryFrame")
-
     def setBrowseEntries(self, **kwargs):
         print ("setBrowseEntries")
-
-
     def setViewEntry(self):
         print ("setViewEntry")
 
