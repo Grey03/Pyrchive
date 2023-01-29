a13bad24766000a2f61deee7d39c3ad1e6e4e8af
Grey03 <greyeag3@gmail.com>
2023-01-29 00:33:43 -0500

    fixed thingssss

--- Pyrchive.py
+++ Pyrchive.py
@@ -1,8 +1,9 @@
-import datetime, json, os
+import datetime, json, os, random
 
 class archivemanager:
     def __init__(self):
         self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self.entriesList = []
     class taggroup:
         def __init__(self):
             #tag groups should be a grouping of tags that are similar to each other
@@ -21,8 +22,23 @@ class archivemanager:
             self.notes = ""
             self.uploadDate = (datetime.datetime.now().__str__())
             for key, value in kwargs.items():
-                if hasattr(self, key):
+                if hasattr(self, key) and key != "tags":
                     setattr(self, key, value)
+                elif key == "tags":
+                    self.setTags(value)
+        def setTags(self, tags):
+            if type(tags) != list: raise TypeError("Tags must be a list")
+            for i in range(len(tags)):
+                tags[i]=tags[i].lower()
+                tags[i] = tags[i].replace("-", "")
+                tags[i] = tags[i].replace("\n", "")
+            tags = ([*set(tags)])
+            tags.sort()
+            self.tags = tags
+            self.tags.sort()
+        def dictToArchiveEntry(self, dictionary):
+            for key, value in dictionary.items():
+                self.__dict__[key] = value
         def __str__(self):
             #Returns a string representation of the dictionary of the object
             return json.dumps(self.__dict__(), indent=4, sort_keys=True)
@@ -52,3 +68,53 @@ class archivemanager:
                 if positiveTag not in self.tags:
                     return False
             return True
+    def getID(self):
+        if len(self.entriesList) == 0:
+            return 0
+        for position, entry in enumerate(self.entriesList):
+            if position != entry["ID"]:
+                return position
+        return len(self.entriesList)
+    def modifyEntry(self, entryID, newData):
+        try: self.entriesList[entryID] = newData
+        except: return False
+    def addEntry(self, entry):
+        try:
+            self.entriesList.insert(entry["ID"], entry)
+        except:
+            self.entriesList.append(entry)   
+    def refresh(self):
+        self.saveEntries()
+        self.loadEntries()
+    def loadEntries(self):
+        try:
+            with open(self.__location__ + "/pyrchiveFolders/pyrchiveJsonData/ArchiveEntries.json", "r") as f:
+                temp = json.load(f)
+                self.entriesList = temp
+                return temp
+        except:
+                self.entriesList = []
+    def saveEntries(self):
+        with open(self.__location__ + "/pyrchiveFolders/pyrchiveJsonData/ArchiveEntries.json", "w") as f:
+            json.dump(self.entriesList, f, indent=4)
+    def createTestEntry(self, **kwargs):
+        count = 1
+        for key, value in kwargs.items():
+            if key == "count": count = value
+        for i in range(count):
+            tags = [random.choice(["tag1", "tag2", "tag3", "tag4", "tag5"]), random.choice(["tag1", "tag2", "tag3", "tag4", "tag5"]), random.choice(["tag1", "tag2", "tag3", "tag4", "tag5"])]
+            tempFile = self.archiveentry(ID=self.getID(), title="title", tags=tags)
+            print ((tempFile.__dict__()), end="\r")
+            self.addEntry(tempFile.__dict__())
+
+
+            
+
+
+
+
+
+test = archivemanager()
+test.loadEntries()
+test.saveEntries()
+
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import customtkinter, os
+import customtkinter, os, shutil
 from pyrchive import archivemanager
 from tkinter import filedialog, messagebox
 from tktooltip import ToolTip
+from PIL import Image, ImageTk
 
 global __location__
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
 
 class App(customtkinter.CTk):         
     def __init__(self):
         super().__init__()
         self.title("Tarchive")
         self.geometry(f"{1135}x{925}")
+        self.minsize(950, 700)
+
         self.Archive = archivemanager()
         self.pageIndex = 0
 
-        def TopFrame():
+        def TopFrame(self):
             #Top Buttons Frame
             self.optionButtonsFrame=customtkinter.CTkFrame(self)
             #-+-+-+-+-+-
@@ -28,29 +31,31 @@ class App(customtkinter.CTk):
             self.uploadButton.pack(side="left", padx=5,pady=5)
             self.savedSearchesDropdown=customtkinter.CTkOptionMenu(self.optionButtonsFrame, values=["Saved Searches: "])
             self.savedSearchesDropdown.pack(side="left", padx=5,pady=5)
-            self.showAllTagsButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Show All Tags")
-            self.showAllTagsButton.pack(side="left", padx=5,pady=5)
+            #self.showAllTagsButton=customtkinter.CTkButton(self.optionButtonsFrame,text="Show All Tags")
+            #self.showAllTagsButton.pack(side="left", padx=5,pady=5)
             self.randomButton=customtkinter.CTkButton(self.optionButtonsFrame, text= "Random")
             self.randomButton.pack(side="left",padx=5,pady=5)
-            self.refreshButton=customtkinter.CTkButton(self.optionButtonsFrame, text=r"↻", width=10, height=10)
+            self.refreshButton=customtkinter.CTkButton(self.optionButtonsFrame, text=r"↻", font=("Roboto", 16, "bold"), width=30, height=30)
             self.refreshButton.pack(side="right",padx=5,pady=5)
             self.fileCountLabel=customtkinter.CTkLabel(self.optionButtonsFrame,text=f"Total File Count: {9}")
             self.fileCountLabel.pack(side="left",padx=5,pady=5)
             self.optionButtonsFrame.pack(padx=10, pady=5, fill="x")
-        TopFrame()
-        def SearchFrame():
+        TopFrame(self)
+        def SearchFrame(self):
             #Search Frame
             self.searchFrame = customtkinter.CTkFrame(self)
-            self.saveButton = customtkinter.CTkButton(self.searchFrame, text="Save",width=50)
+            self.saveButton = customtkinter.CTkButton(self.searchFrame, text="💾", font=("Roboto", 16, "bold"), width=30, height=30)
             self.saveButton.pack(side="left")
             self.searchBar= customtkinter.CTkEntry(self.searchFrame,placeholder_text="Search")
             self.searchBar.bind("<Return>")
             self.searchBar.pack(padx=5,side="left", fill="x",expand=True)
+            self.clearButton = customtkinter.CTkButton(self.searchFrame, text="Clear", width=80, command = lambda: self.searchBar.delete("0", "end"))
+            self.clearButton.pack(side="left", padx=5, pady=5)
             self.searchButton = customtkinter.CTkButton(self.searchFrame, text="Search", width=80)
-            self.searchButton.pack(side="left")
+            self.searchButton.pack(side="left", padx=5, pady=5)
             self.searchFrame.pack(padx=10, pady=5,fill="x")
-        SearchFrame()
-        def BottomFrame():
+        SearchFrame(self)
+        def BottomFrame(self):
             #Bottom Frame
             self.bottomFrame=customtkinter.CTkFrame(self, fg_color="transparent")
             self.tagFrame=customtkinter.CTkFrame(self.bottomFrame, width=200)
@@ -59,10 +64,34 @@ class App(customtkinter.CTk):
             self.tagList = customtkinter.CTkFrame(self.tagFrame)
             self.tagList.pack(padx=5,pady=5, expand=True, fill="both")
             self.tagFrame.pack(side="left", fill="y")
-            self.entryFrame=customtkinter.CTkFrame(self.bottomFrame, fg_color="transparent")
-            self.entryFrame.pack(side="left",fill="both", expand=True)
+            self.mainFrame=customtkinter.CTkFrame(self.bottomFrame, fg_color="transparent")
+            self.mainFrame.pack(side="left",fill="both", expand=True)
             self.bottomFrame.pack(fill="both",expand=True,padx=10,pady=5)
-        BottomFrame()
+        BottomFrame(self)
+
+        def clearMainFrame(self):
+            for child in self.mainFrame.winfo_children():
+                child.destroy()
+        def entryBrowseScreen(self):
+            self.clearMainFrame()
+            print ("Browsing Entries")
+        def entryViewScreen(self, entry):
+            self.clearMainFrame()
+            print ("Viewing an Entry")
+
+            mediaWindow = customtkinter.CTkFrame(self.mainFrame)
+            #run function to either display a video or an image
+            mediaWindow.pack(fill="both", expand=True, padx=10, pady=10)
+
+            infoFrame = customtkinter.CTkFrame(self.mainFrame)
+            infoFrame.pack(fill="both", expand=True, padx=10, pady=10)
+
+            bottomButtonFrame = customtkinter.CTkFrame(self.mainFrame)
+            bottomButtonFrame.pack(fill="both", expand=True, padx=10, pady=10
+
+
+
+
 
 
 if __name__ == "__main__":
